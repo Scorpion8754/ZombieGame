0a26caab63edd4cc78474289b0eb47d13066ba3b
Shane <shanemain8754@gmail.com>
2018-02-14 08:17:32 -0800

    Started sprite work, added new zombie class, improved movement and attack logic

--- _Main.py
+++ _Main.py
@@ -6,7 +6,7 @@ import math
 pygame.init()
 gameWidth = 1280
 gameHeight = 720
-gameWindow = pygame.display.set_mode((gameWidth,gameHeight))
+gameWindow = pygame.display.set_mode((gameWidth, gameHeight))
 pygame.display.set_caption("ZOMBIE GAME")
 clock = pygame.time.Clock()
 
@@ -18,6 +18,7 @@ white = (255, 255, 255)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
+yellow = (153, 153, 0)
 #Globals
 bullets = []
 zombies = []
@@ -35,32 +36,44 @@ def calcMove(speed, fromx, fromy, tox, toy):  # I'm going this fast, want to get
 
 
 
-class Zombie():
+
+class Zombie(pygame.sprite.Sprite):
     def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((15, 15))
         self.image.fill(red)
+        self.strength = 10
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(0, gameWidth)
         self.rect.y = random.randrange(0, gameHeight)
-        self.speed = 2
+        self.speed = 2.3
         self.cd = 0
-        self.cdMax = 50
+        self.cdMax = 30
     def move(self):
-        mvmt = calcMove(self.speed, self.rect.x, self.rect.y, player.rect.x, player.rect.y)
-        print(mvmt)
-        if self.cd <= 0:
-            self.rect.x = mvmt[0]
-            self.rect.y = mvmt[1]
         xdiff = self.rect.x - player.rect.x
         ydiff = self.rect.y - player.rect.y
-        if(xdiff >= -15) and (xdiff <= 15) and (ydiff >= -15) and (ydiff <= 15):
-            self.attack()
+        if(xdiff < -8) or (xdiff > 8):
+            mvmt = calcMove(self.speed, self.rect.x, self.rect.y, player.rect.x, player.rect.y)
+            if self.cd < 0:
+                self.rect.x = mvmt[0]
+                self.rect.y = mvmt[1]
+                if(xdiff >= -15) and (xdiff <= 15) and (ydiff >= -15) and (ydiff <= 15):
+                    self.attack()
     def attack(self):
         print("You got slapped!")
         self.cd = self.cdMax
     def update(self):
         self.cd -= 1
 
+class bigZombie(Zombie):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((20, 20))
+        self.image.fill(yellow)
+        self.speed = 1.5
+    def attack(self):
+        print("You got bitch slapped!")
+        self.cd = self.cdMax
 
 class playerActive():
     def __init__(self):
@@ -69,14 +82,14 @@ class playerActive():
         self.rect = self.image.get_rect()
         self.rect.x = 100
         self.rect.y = 100
-        self.speed = 3
+        self.speed = 4
         self.ammo = 100
         self.cd = 10
         self.cdMax = 10
+        self.isHealing = False
     def move(self, xdir, ydir):
         self.rect.x += xdir*self.speed
         self.rect.y += ydir*self.speed
-        print(self.rect.x, self.rect.y)
     def shoot(self):
         if self.cd <= 0 and self.ammo:
             self.cd = self.cdMax
@@ -90,11 +103,14 @@ class playerActive():
 
 
 
+all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group
 player = playerActive()
-zombies.append(Zombie())
-zombies.append(Zombie())
-zombies.append(Zombie())
-zombies.append(Zombie())
+for i in range(2):
+    z = Zombie()
+    all_sprites.add(z)
+    mobs.add(z)
+zombies.append(bigZombie())
 
 
 #Game Start
@@ -128,10 +144,12 @@ while gameActive:
         obj.travel
     #Run updates to handle cooldowns etc.
     player.update()
+    all_sprites.update()
 
     gameWindow.fill(white)
     #Draw stuff here
     gameWindow.blit(player.image, player.rect)
+    all_sprites.draw(gameWindow)
     for i in range(len(zombies)):
         gameWindow.blit(zombies[i].image, zombies[i].rect)
         zombies[i].move()
