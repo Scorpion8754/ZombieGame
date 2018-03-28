10ecb95a78731af6bd2043395aac00611e5f6922
Scorpion8754 <35542934+Scorpion8754@users.noreply.github.com>
2018-03-28 16:30:27 -0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,31 @@
+import math
+# define some colors (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# game settings
+WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16
+HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
+FPS = 60
+TITLE = "Zombie Game"
+BGCOLOR = DARKGREY
+
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+
+def calcMove(speed, fromx, fromy, tox, toy):  # I'm going this fast, want to get from(x,y) to(x,y)
+    run = fromx - tox
+    rise = toy - fromy
+    length = math.sqrt((rise * rise) + (run * run))
+    unitx = run / length
+    unity = rise / length
+    fromx -= int(unitx * speed)
+    fromy += int(unity * speed)
+    return (fromx, fromy)
